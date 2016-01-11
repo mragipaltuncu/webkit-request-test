64167cd01438b3862cedff57e897bd06842a3df1
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-10 23:43:48 -0800

    webKit test

--- webRequestTest/ViewController.swift
+++ webRequestTest/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    
+    
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        webView = WKWebView()
+        container.addSubview(webView)
+    
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
+        
+    }
+    
+    
+    func loadRequest(urlString : String) {
+        
+      
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+        
+    }
+    
+    @IBAction func loadSwift(sender: AnyObject) {
+        
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loadSteak(sender: AnyObject) {
+        
+        loadRequest("http://www.foodrepublic.com/2013/01/23/what-your-steak-really-says-about-you/")
+        
+        
     }
-
+    
+    @IBAction func loadGossip(sender: AnyObject) {
+        
+        loadRequest("http://www.tmz.com")
+    }
 
 }
 
