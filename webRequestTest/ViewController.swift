b2f1edf228bcae64d0db0167a683f3908393915c
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-10 23:14:17 -0800

    Initial Commit

---
+++ webRequestTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webRequestTest
+//
+//  Created by Mehmet Ragıp Altuncu on 10/01/16.
+//  Copyright © 2016 MehmetAltuncu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
